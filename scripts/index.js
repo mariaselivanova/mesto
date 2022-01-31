f43e252eb08479e015bb9429bc9c21b87b13e154
Maria Selivanova <maria.m.selivanova@yandex.ru>
2022-01-31 13:40:44 +0300

    исправления после проверки

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,29 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const popupopenedClass = 'popup_opened';
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_profession');
+let paragraphName = document.querySelector('.profile__name');
+let paragraphJob = document.querySelector('.profile__profession');
 
-buttonEdit.addEventListener('click', function (event) {
-  event.preventDefault();
+function openpopup() {
   popup.classList.add(popupopenedClass);
   nameInput.value = paragraphName.textContent;
   jobInput.value = paragraphJob.textContent;
-});
+}
 
-buttonClose.addEventListener('click', function (event){
-  event.preventDefault();
+function closepopup(){
   popup.classList.remove(popupopenedClass);
-});
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__profession');
-let paragraphName = document.querySelector('.profile__name');
-let paragraphJob = document.querySelector('.profile__profession');
+}
 
 function formSubmitHandler (event) {
     event.preventDefault();
     paragraphName.textContent = nameInput.value;
     paragraphJob.textContent = jobInput.value;
-    popup.classList.remove(popupopenedClass);
+    closepopup();
 }
-formElement.addEventListener('submit', formSubmitHandler);
 
+buttonEdit.addEventListener('click', openpopup);
+buttonClose.addEventListener('click', closepopup);
+formElement.addEventListener('submit', formSubmitHandler);
